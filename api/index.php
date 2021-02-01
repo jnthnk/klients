b0f0df5c72e59ddf19cf8a71ad47e45f2694ab68
Jonathan Kim <jnthnk@outlook.com>
2021-02-01 16:30:33 +0100

    Check routing and return JSON

--- api/index.php
+++ api/index.php
@@ -1,20 +1,87 @@
 <?php
 
 
+// Try to connect to a database called 'klients' on localhost
+// This database must be created before in phpmyadmin
+// Send a JSON error message if authentication fails
+
+try {
+  
+  $data = new PDO('mysql:host=localhost;dbname=klients', 'root', '');
+  
+} catch (PDOException $_error) {
+  
+  header('Content-Type:application/json');
+  header('HTTP/1.0 500 Internal Server Error');
+  
+  echo json_encode([
+    'message' => 'Database Connection Error: '.$_error->getMessage()
+  ]);
+  
+  exit;
+  
+}
+
+
 // 
 
-$response = [];
+$_path = $_GET['path'] ?? '';
+
+$json_request = 'NONE';
+
+if ($_path) {
+  
+  $_paths = explode('/', $_path);
+  
+  if ($_paths[0] === 'clients') {
+    
+    $json_request = 'GET ALL';
+    
+  } elseif ($_paths[0] === 'client') {
+    
+    $_subpath = $_paths[1] ?? '';
+    
+    $_method = $_SERVER['REQUEST_METHOD'];
+    
+    if ($_subpath) {
+      
+      
+      
+    } else {
+      
+      if ($_method === 'POST') {
+        
+        $json_request = 'ADD ONE';
+        
+      } else {
+        
+        $json_request = 'GET ONE';
+        $json_message = 'To get certain client data, provide the ID';
+        
+      }
+      
+    }
+    
+  }
+  
+}
 
 
 // 
 
-global $_GET;
 
-echo $_GET['path'];
 
 
 // 
 
-//$database = new PDO('mysql:host=localhost;dbname=klients', 'root', '');
+$json = [
+  'request' => $json_request,
+  'message' => $json_message,
+];
 
+// 
+
+header('Content-Type:application/json');
+
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
 
