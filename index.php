3dfd8edb7764ea74327de50ddf56255ef5231714
hidekimaki <hidekimaki@hotmail.com>
2021-02-02 12:51:24 -0300

    html e css inicial

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="utf-8">
+    <title>Cadastro de Cliente</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h3>Cadastro de Clientes</h3>
+    <section id="formulario">
+        <form>
+            <h2>Cadastro</h2>
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome">
+            <label>CPF</label>
+            <input type="text" name="CPF" id="CPF">
+            <label>Data Nasc</label>
+            <input type="text" name="DTN" id="DTN">
+        </form>
+    </section>
+
+    <section id="tabela">
+        <table>
+            <tr id="Titulo">
+                <td>ID</td>
+                <td>NOME</td>
+                <td>CPF</td>
+                <td colspan="2">DATA NASC</td>
+            </tr>
+
+            
+
+        
+        </table>
+
+    </section>
+
+</body>
+</html>
