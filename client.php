0d7bcdc146ba55a8c35be501ed18fe138238dd37
hidekimaki <hidekimaki@hotmail.com>
2021-02-04 10:07:48 -0300

    excusão de front temp

--- client.php
+++ client.php
@@ -1,18 +1,157 @@
-<head> 
-    <meta charset="utf-8">
-    <title>Cadastro de Cliente</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php
 
-<section id="formulario">
-        <form action="http://localhost/klients/api/client" method="post" target="_blank">
-            <h2>Cadastro</h2>
-            <label for="nome">Nome</label>
-            <input type="text" name="name" id="name">
-            <label for='cpf'>CPF</label>
-            <input type="text" name="cpf" id="cpf">
-            <label for='date'>Data Nascimento (YYYY-MM-DD)</label>
-            <input type="text" name="date" id="date">
-            <input type="submit" value="OK">
-        </form>
-</section>
+
+// 
+
+$post_ID = $_GET['ID'] ?? '';
+
+if ($post_ID) {
+  $post_ID = intval($post_ID);
+}
+
+// 
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => 'http://127.0.0.1/klients/api/client'.($post_ID ? '/'.$post_ID : ''),
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTPHEADER => ['Content-type: application/json']
+]);
+
+$data = curl_exec($curl);
+
+$data = json_decode($data, true);
+
+
+// 
+
+$page_title = 'Cadastrar Novo Cliente';
+
+
+// 
+
+if ($data['client'] ?? '') {
+  $page_button = 'Salvar alteracoes';
+} else {
+  $page_button = 'Adicionar novo';
+}
+
+
+// 
+
+$page = [
+  'title' => $page_title,
+  'button' => $page_button,
+];
+
+
+?><!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <title>Klients v1.0</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/reset-2.css">
+    <link rel="stylesheet" href="assets/styles-2.css">
+  </head>
+  <body>
+    <div class="wrap">
+      <header class="header">
+        <?php if ($data['client'] ?? null) { ?>
+          <h1 class="title is-large">Editar cliente:</h1>
+          <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident incidunt magni ipsa possimus dolorum numquam aliquid porro odio velit aliquam!</p>
+        <?php } else { ?>
+          <h1 class="title is-large">Cadastrar novo cliente:</h1>
+          <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident incidunt magni ipsa possimus dolorum numquam aliquid porro odio velit aliquam!</p>
+        <?php } ?>
+      </header>
+      <form class="content">
+        <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, rerum.</p>
+        <ul class="grid">
+          <?php if ($data['client'] ?? null) { ?>
+            <li class="grid-item no-1">
+              <div class="content-control is-active">
+                <label class="text has-dots" for="name">Nome completo:</label>
+                <input class="input" id="name" name="name" type="text" value="<?php echo $data['client']['name']; ?>" placeholder="Insira o nome aqui..." required>
+              </div>
+            </li>
+            <li class="grid-item no-2">
+              <div class="content-control is-active">
+                <label class="text has-dots" for="cpf">CPF:</label>
+                <input class="input" id="cpf" name="cpf" type="text" value="<?php echo $data['client']['CPF']; ?>" placeholder="Insira o CPF aqui..." required>
+              </div>
+            </li>
+            <li class="grid-item no-3">
+              <div class="content-control is-active">
+                <label class="text has-dots" for="date">Data de nascimento:</label>
+                <input class="input" id="date" name="date" type="date" value="<?php echo date('Y-m-d', $data['client']['date']); ?>" placeholder="dd/mm/yyyy" required>
+              </div>
+            </li>
+          <?php } else { ?>
+            <li class="grid-item no-1">
+              <div class="content-control is-active">
+                <label class="text has-dots" for="name">Nome completo:</label>
+                <input class="input" id="CPF" name="CPF" type="text" placeholder="Insira o nome aqui..." required>
+              </div>
+            </li>
+            <li class="grid-item no-2">
+              <div class="content-control is-active">
+                <label class="text has-dots" for="CPF">CPF:</label>
+                <input class="input" id="CPF" name="CPF" type="text" placeholder="Insira o CPF aqui..." required>
+              </div>
+            </li>
+            <li class="grid-item no-3">
+              <div class="content-control is-active">
+                <label class="text has-dots" for="name">Data de nascimento:</label>
+                <input class="input" type="date" placeholder="dd/mm/yyyy" required>
+              </div>
+            </li>
+          <?php } ?>
+        </ul>
+        <nav class="content-options">
+          <ul class="grid">
+            <?php if ($data['client'] ?? null) { ?>
+              <li class="grid-item no-1 is-left">
+                <a class="button is-red" href="#">Remover</a>
+              </li>
+              <li class="grid-item no-2 is-right">
+                <button class="button is-green" type="submit" onclick="SubForm(<?php echo $id; ?>)">Salvar alterações</button>
+              </li>
+            <?php } else { ?>
+              <li class="grid-item no-1 is-right">
+                <button class="button is-green" type="submit">Adicionar novo</button>
+              </li>
+            <?php } ?>
+          </ul>
+        </nav>
+      </form>
+    </div>
+    <footer class="copyright">
+      <p class="text is-small">Lorem ipsum dolor sit</p>
+      <p class="text is-small">Klients v1.1</p>
+    </footer>
+  </body>
+
+
+  <script>
+                function SubForm (id){
+                    console.log(id);
+                    $.ajax({
+                        url: 'http://localhost/klients/api/client/'+id ,
+                        type: 'POST',
+                        data: jQuery.param({
+                            "name": document.getElementById('name').value,
+                            "cpf":  document.getElementById('cpf').value,
+                            "date": document.getElementById('date').value
+                        } ),
+                        
+                        //$('atualiza').serialize(),
+                        success: function(data){
+                            alert("worked");console.log(data);
+                            
+                        }
+                    });
+                }
+            </script>
+</html>
--- index.php
+++ index.php
@@ -1,63 +1,78 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head> 
+<?php
+
+
+// 
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => 'http://127.0.0.1/klients/api/clients/',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTPHEADER => ['Content-type: application/json']
+]);
+
+$data = curl_exec($curl);
+
+$data = json_decode($data, true);
+
+
+// 
+
+$page = [
+  'clients' => $data['clients'],
+];
+
+
+?><!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <title>Klients v1.0</title>
     <meta charset="utf-8">
-    <title>Cadastro de Cliente</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    
-    <section id="formulario">
-        <form action="http://localhost/klients/client.php">
-            <h2>Controle de Clientes</h2>
-            <input  type="button" value="Cadastrar">
-        </form>
-    </section>
-
-    <section id="tabela">
-        <table>
-            <tr id="Titulo">
-                <td>ID</td>
-                <td>NOME</td>
-                <td>CPF</td>
-                <td colspan="2">DATA NASC</td>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles-2.css">
+  </head>
+  <body>
+    <main class="wrap">
+      <header class="header">
+        <h1 class="title is-large">Ver todos os clientes:</h1>
+        <p class="text is-large">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident incidunt magni ipsa possimus dolorum numquam aliquid porro odio velit aliquam!</p>
+      </header>
+      <section class="content">
+        <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, rerum.</p>
+        <table class="content-table">
+          <thead class="content-t-head">
+            <tr class="grid">
+              <th class="grid-item no-1">ID</th>
+              <th class="grid-item no-2">Nome:</th>
+              <th class="grid-item no-3">CPF:</th>
+              <th class="grid-item no-4">Data de nasc.:</th>
             </tr>
-            
-            <?php
-                $request = array(
-                    CURLOPT_URL => "http://localhost/klients/api/clients",
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_TIMEOUT => 30,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => "GET",
-                    CURLOPT_HTTPHEADER => array("cache-control: no-cache")
-                );
-
-                $curl = curl_init();
-                
-                curl_setopt_array($curl, $request);
-                
-                $response = curl_exec($curl);
-                $err = curl_error($curl);
-                curl_close($curl);
-
-                if(!$err){
-                    $lista = json_decode($response,true);
-                    
-                    foreach ($lista['clients'] as $key => $linha) {
-                        echo
-                            '<tr id="Titulo">
-                                <td>'.$linha['ID'].'</td>
-                                <td>'.$linha['name'].'</td>
-                                <td>'.$linha['CPF'].'</td>
-                                <td>'.$linha['date'].'</td>
-                                <td><input  type="submit" value="Editar"><input  type="submit" value="Excluir"></td>
-                            </tr>';
-                    }
-                }
-            ?>
+          </thead>
+          <?php if ($page['clients']) { ?>
+            <tbody class="content-t-body">
+              <?php foreach ($page['clients'] as $client) { ?>
+                <tr class="grid">
+                  <td class="grid-item no-1"><?php echo $client['ID'] ?></td>
+                  <td class="grid-item no-2"><a class="link" href="client.php?ID=<?php echo $client['ID'] ?>"><?php echo $client['name'] ?></a></td>
+                  <td class="grid-item no-3"><?php echo $client['CPF'] ?></td>
+                  <td class="grid-item no-4"><?php echo date('d/m/Y', $client['date']) ?></td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          <?php } ?>
         </table>
-    </section>
-
-</body>
+        <nav class="content-options">
+          <ul class="grid">
+            <li class="grid-item is-right">
+              <a class="button is-green" href="client.php">Cadastrar Novo Cliente</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </main>
+    <footer class="copyright">
+      <p class="text is-small">Lorem ipsum dolor sit</p>
+      <p class="text is-small">Klients v1.1</p>
+    </footer>
+  </body>
 </html>
